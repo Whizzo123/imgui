500640450e666d6efe599bd517da3ff2be6b7be1
joe_scobbie <joseph.scobbie@outlook.com>
2025-05-21 07:31:26 +0100

    Updated Premake
    Premake now includes imgui_stdlib

--- premake5.lua
+++ premake5.lua
@@ -6,6 +6,8 @@ project "ImGui"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+    includedirs { "../*.h" }
+
 	files
 	{
 		"imconfig.h",
@@ -18,7 +20,9 @@ project "ImGui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+        "misc/cpp/imgui_stdlib.h",
+        "misc/cpp/imgui_stdlib.cpp"
 	}
 
 	filter "system:windows"
